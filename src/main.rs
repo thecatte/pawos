a1e0644cc5c5d624f37fa78e1c746e9a6ae9cb32
Paweł Trojański <pawel.trojanski@outlook.com>
2020-11-20 02:08:04 +0100

    interrupts, breakpoint exception handling

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,13 @@ use pawos::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    pawos::init();
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
